093c82df49042f07cd770debb385f0057c149e77
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-05-18 21:04:33 +0700

    foreach

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -6,11 +6,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertEqualsCanonicalizing;
+
 class CollectionTest extends TestCase
 {
     public function testCreateCollection()
     {
         $collection = collect([1, 2, 3]);
-        $this->assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+        assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+    }
+
+    public function testForEach()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        foreach ($collection as $key => $value) {
+            assertEquals($key + 1, $value);
+        }
     }
 }
